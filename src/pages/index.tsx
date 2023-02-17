e3f1cdccd2991b79e64ab79e6ea4ea06b320b910
Shadman Ali <shadman.ali@live.co.uk>
2023-02-17 11:38:31 +0530

    removes tailwind elements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
-import 'tw-elements';
+// import 'tw-elements';
 
 const inter = Inter({ subsets: ["latin"] });
 
