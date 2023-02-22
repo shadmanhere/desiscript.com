fc0e05f68bba2594a11db0c44f6912b1dc628e6d
Shadman Ali <shadman.ali@live.co.uk>
2023-02-22 18:08:29 +0530

    html update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import Link from 'next/link'
+import Link from "next/link";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import { useEffect, useState } from "react";
@@ -26,49 +26,52 @@ export default function Home() {
   }, []);
 
   return (
-    <div className={styles.header}>
-      <Head>
-        <title>Home | Desi Script</title>
-        <meta name="description" content="Full Stack Developer" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <header>
-        <nav className={styles.navitems}>
-          <ul>
-            <li>
-              <Link style={{ fontSize: "1.8rem" }} href="/">
-                Desi Script
-              </Link>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <a href="#home">Home</a>
-            </li>
-            <li>
-              <a href="#services">Services</a>
-            </li>
-            <li>
-              <a href="#skills">Skills</a>
-            </li>
-            <li>
-              <a href="#projects">Projects</a>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main className={styles.main}>
+    <>
+      <div className={styles.header}>
+        <Head>
+          <title>Home | Desi Script</title>
+          <meta name="description" content="Full Stack Developer" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <header>
+          <nav className={styles.navitems}>
+            <ul>
+              <li>
+                <Link style={{ fontSize: "1.8rem" }} href="/">
+                  Desi Script
+                </Link>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <a href="#home">Home</a>
+              </li>
+              <li>
+                <a href="#services">Services</a>
+              </li>
+              <li>
+                <a href="#skills">Skills</a>
+              </li>
+              <li>
+                <a href="#projects">Projects</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <section id="home" className={styles.home}>
-          <p>Let&apos;s build your next project</p>
+          <h1>Let&apos;s build your next project</h1>
 
-          <TextTransition className={styles.animatedText} springConfig={presets.gentle}>
+          <TextTransition
+            className={styles.animatedText}
+            springConfig={presets.gentle}
+          >
             {TEXTS[index % TEXTS.length]}
           </TextTransition>
 
@@ -80,7 +83,10 @@ export default function Home() {
             <button className={styles.button}>Contact</button>
           </a>
         </section>
-      </main>
-    </div>
+      </div>
+      <section id="services" className={`container`}>
+        <h2>Services</h2>
+      </section>
+    </>
   );
 }
