be32bf9c898802bb41d1f050245ffc10ce8861b2
Shadman Ali <shadman.ali@live.co.uk>
2023-03-06 11:36:28 +0530

    makes services section responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -87,14 +87,14 @@ export default function Home() {
           </a>
         </section>
       </div>
-      <section id="services" className={styles.services}>
-        <h2>Services</h2>
-        <p>
+      <section id="services" className={`${styles.services} xs:px-5 md:px-20 py-16`}>
+        <h2 className="text-7xl font-bold antialiased">Services</h2>
+        <p className="text-3xl my-4">
           I strive to provide quality web-based software and ensure great
           experiences for all users by:
         </p>
-        <div className={styles.servicesContainer}>
-          <div className={styles.flipCard}>
+        <div className="flex justify-between items-center flex-wrap lg:mx-24 xs:flex-col lg:flex-row">
+          <div className={`${styles.flipCard}  xs:w-full xs:h-44 md:w-5/12 lg:h-96`}>
             <div className={styles.flipCardInner}>
               <div className={styles.flipCardFront}>
                 <Image
@@ -107,12 +107,12 @@ export default function Home() {
                     height: "100%",
                   }}
                 />
-                <p style={{ background: "black", color: "white" }}>
+                <p className="text-2xl py-2" style={{ background: "black", color: "white" }}>
                   Front End Development
                 </p>
               </div>
               <div className={styles.flipCardBack}>
-                <p>
+                <p className="xs:text-base md:text-3xl xs:my-2 md:my-4">
                   Collaborating with other stakeholders and discussing the
                   needs, design, and functionality of a website or UI for
                   creating quality mockups and prototypes; ensuring high-quality
@@ -122,7 +122,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className={styles.flipCard}>
+          <div className={`${styles.flipCard} xs:w-full xs:h-44 md:w-5/12 lg:h-96`}>
             <div className={styles.flipCardInner}>
               <div className={styles.flipCardFront}>
                 <Image
@@ -135,12 +135,12 @@ export default function Home() {
                     height: "100%",
                   }}
                 />
-                <p style={{ background: "black", color: "white" }}>
+                <p className="text-2xl py-2" style={{ background: "black", color: "white" }}>
                   Back End Development
                 </p>
               </div>
               <div className={styles.flipCardBack}>
-                <p>
+                <p className="xs:text-base md:text-3xl xs:my-2 md:my-4">
                   Developing and overseeing testing schedules in client-server
                   environment, addressing various browsers and devices to ensure
                   that content is displayed as intended and that the site is
@@ -150,7 +150,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className={styles.flipCard}>
+          <div className={`${styles.flipCard} xs:w-full xs:h-44 md:w-5/12 lg:h-96`}>
             <div className={styles.flipCardInner}>
               <div className={styles.flipCardFront}>
                 <Image
@@ -163,12 +163,12 @@ export default function Home() {
                     height: "100%",
                   }}
                 />
-                <p style={{ background: "black", color: "white" }}>
+                <p className="text-2xl py-2" style={{ background: "black", color: "white" }}>
                   Web Design
                 </p>
               </div>
               <div className={styles.flipCardBack}>
-                <p>
+                <p className="xs:text-base md:text-3xl xs:my-2 md:my-4">
                   Designing user interfaces for web applications, using standard
                   HTML/CSS practices and technologies. Creating visual elements
                   that users see and interact with in web applications.
@@ -177,7 +177,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className={styles.flipCard}>
+          <div className={`${styles.flipCard} xs:w-full xs:h-44 md:w-5/12 lg:h-96`}>
             <div className={styles.flipCardInner}>
               <div className={styles.flipCardFront}>
                 <Image
@@ -190,12 +190,12 @@ export default function Home() {
                     height: "100%",
                   }}
                 />
-                <p style={{ background: "black", color: "white" }}>
+                <p className="text-2xl py-2" style={{ background: "black", color: "white" }}>
                   Hosting Service Web Analytics
                 </p>
               </div>
               <div className={styles.flipCardBack}>
-                <p>
+                <p className="xs:text-base md:text-3xl xs:my-2 md:my-4">
                   Providing hosting services for web applications and web
                   analytics to track the performance of the website and its
                   users.
