242bc9a69c4ff2c6ad7c44f0e5dac9888aeb278a
Shadman Ali <shadman.ali@live.co.uk>
2023-03-08 02:46:34 +0530

    adds new project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -331,7 +331,7 @@ export default function Home() {
         <h2 className="xs:text-5xl md:text-7xl font-bold antialiased">
           Highlighted Projects
         </h2>
-        <div className="flex flex-wrap xs:gap-y-6 justify-evenly my-9">
+        <div className="flex flex-wrap gap-6 justify-evenly my-9">
           {/* issue finder */}
           <div
             className={`${styles.imageContainer} xs:w-full md:w-8/12 lg:w-4/12 xs:h-96 lg:h-80`}
@@ -397,6 +397,8 @@ export default function Home() {
             </div>
           </div>
 
+         {/* ends issue finder */}
+
           {/* learning path */}
 
           <div
@@ -458,6 +460,68 @@ export default function Home() {
               </div>
             </div>
           </div>
+          {/* ends leearning path */}
+
+          {/* the drink bank  */}
+          <div
+            className={`${styles.imageContainer} xs:w-full md:w-8/12 lg:w-4/12 xs:h-96 lg:h-80`}
+          >
+            <Image
+              src="/images/projects/drinkbank.png"
+              alt="Avatar"
+              width="800"
+              height="800"
+              className={styles.image}
+            />
+            <div className={styles.overlay}>
+              <div className={`${styles.overlayText} text-justify my-2`}>
+                <h3 className="mt-0 mb-2 text-3xl font-bold leading-tight text-white opacity-100">
+                The Drink Bank
+                </h3>
+                <hr className="my-2 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
+                <p className="my-4">
+                  Find your favorite drink here.
+                </p>
+                <a
+                  href="https://github.com/shadmanhere/drink_bank"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button
+                    type="button"
+                    className="mr-2 inline-block rounded border-2 border-white px-6 pt-2 pb-[6px] text-xs font-medium uppercase leading-normal text-white bg-black transition 
+                  duration-150 ease-in-out hover:bg-white hover:text-black
+                  focus:border-neutral-800 focus:text-white focus:outline-none 
+                  focus:ring-0 active:border-white active:text-white dark:border-neutral-900 dark:text-white 
+                  dark:hover:border-neutral-900 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10 dark:hover:text-white dark:focus:border-neutral-900 
+                  dark:focus:text-white dark:active:border-neutral-900 dark:active:text-white"
+                    data-te-ripple-init
+                  >
+                    Code
+                  </button>
+                </a>
+                <a
+                  href="https://xenodochial-liskov-3f1582.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button
+                    type="button"
+                    className="opacity-100 mr-2 inline-block rounded border-2 border-white px-6 pt-2 pb-[6px] text-xs font-medium uppercase leading-normal text-white bg-black transition 
+                  duration-150 ease-in-out hover:bg-white hover:text-black
+                  focus:border-neutral-800 focus:text-white focus:outline-none 
+                  focus:ring-0 active:border-white active:text-white dark:border-neutral-900 dark:text-white 
+                  dark:hover:border-neutral-900 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10 dark:hover:text-white dark:focus:border-neutral-900 
+                  dark:focus:text-white dark:active:border-neutral-900 dark:active:text-white"
+                    data-te-ripple-init
+                  >
+                    Live
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* ends drink bank */}
         </div>
       </section>
       <button
