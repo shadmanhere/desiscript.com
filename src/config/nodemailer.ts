ec298be106ceb24bba177991f8f8ed81fdeda72d
Shadman Ali <shadman.ali@live.co.uk>
2023-03-09 07:24:15 +0530

    adds api to get contact

---
+++ src/config/nodemailer.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+
+interface EmailInterface {
+    host: string;
+  }
+  
+  export default EmailInterface;
+  
+
+export const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD,
+    },
+} as EmailInterface );
+
+export const mailOptions = {
+  from: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`,
+  to: `${process.env.SMTP_FROM_EMAIL}`,
+};
