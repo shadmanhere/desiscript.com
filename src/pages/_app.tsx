590b1f9ae5abee4209443e21582ce1e8f776628e
Shadman Ali <shadman.ali@live.co.uk>
2023-03-10 23:50:52 +0530

    adds google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,37 @@
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-
   useEffect(() => {
     const use = async () => {
-      (await import('tw-elements')).default;
+      (await import("tw-elements")).default;
     };
     use();
   }, []);
-    
-  return <Component {...pageProps} />;
+
+  return (
+    <>
+    <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.REACT_APP_GOOGLE_ANALYTICS_ID}`} />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', ${process.env.REACT_APP_GOOGLE_ANALYTICS_ID}, {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+      <Component {...pageProps} />;
+    </>
+  );
 }
