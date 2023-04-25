cf39fc08fab2886dcef3734887721371982c8968
Shadman Ali <shadman.ali@live.co.uk>
2023-04-25 12:51:56 +0530

    patch: update about section text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,11 +7,16 @@ const About = () => {
         About Me
       </h2>
       <p className="rounded border-8 py-16 px-6 text-2xl shadow-2xl">
-        Passionate about developing websites that are both visually appealing
-        and highly functional. With a background in engineering, I am currently
-        seeking a full-time position with a socially responsible company that
-        values diversity and inclusion and is committed to leveraging technology
-        for positive change.
+        As a skilled full stack web developer, I am passionate about creating
+        visually stunning websites that offer superior functionality. With a
+        background in engineering, I am currently open to full-time and
+        contract positions with socially responsible companies that value
+        diversity and inclusion and are committed to using technology to create
+        positive change. I am driven by the desire to leverage my skills to
+        develop websites that provide an exceptional user experience and promote
+        engagement. Whether you are looking for a full-time web developer to
+        join your team or seeking a contractor to complete a project, I would be
+        delighted to contribute my expertise and help bring your vision to life.
       </p>
     </section>
   );
